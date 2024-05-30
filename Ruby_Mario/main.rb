7bb52b8b813eb0fabc7310375a2a1457304abc7b
Łukasz Chlebek <chlebeke.lukasz@gmail.com>
2024-05-30 12:45:21 +0200

    Ruby 2d Mario - Wymaganie 3.5

--- Ruby_Mario/main.rb
+++ Ruby_Mario/main.rb
@@ -8,7 +8,7 @@ JUMP_POWER = 15
 MOVE_SPEED = 5
 
 class Player
-  attr_reader :x, :y, :width, :height
+  attr_reader :x, :y, :width, :height, :score
 
   def initialize
     @x = 50
@@ -17,6 +17,7 @@ class Player
     @height = 20
     @velocity_y = 0
     @on_ground = false
+    @score = 0 
   end
 
   def draw
@@ -43,12 +44,23 @@ class Player
     @y += @velocity_y
   end
 
-  def update(obstacles, holes)
+  def update(obstacles, holes, points)
     apply_gravity
-    check_collisions(obstacles, holes)
+    check_collisions(obstacles, holes, points)
+    collect_points(points)
   end
 
-  def check_collisions(obstacles, holes)
+  def collect_points(points)
+    points.each do |point|
+      if !point[:collected] && @x + @width / 2 >= point[:x] && @x + @width / 2 <= point[:x] + point[:size] &&
+          @y + @height / 2 >= point[:y] && @y + @height / 2 <= point[:y] + point[:size]
+        @score += 10
+        point[:collected] = true
+      end
+    end
+  end
+
+  def check_collisions(obstacles, holes, points)
     @on_ground = false
     
     obstacles.each do |obs|
@@ -76,16 +88,37 @@ class Player
 
     holes.each do |hole|
       if @x >= hole[:x] && @x + @width <= hole[:x] + hole[:width] && @y + @height >= hole[:y] && @y <= hole[:y] + hole[:height]
-        reset_position
+       reset_position(points)
       end
     end
   end
 
-  def reset_position
+  def reset_position(points)
     @x = 50
     @y = 300
     @velocity_y = 0
     @on_ground = false
+    @score = 0  
+    points.each { |point| point[:collected] = false } 
+  end
+end
+
+class Point
+  attr_reader :collected
+
+  def initialize(x, y, size)
+    @x = x
+    @y = y
+    @size = size
+    @collected = false
+  end
+
+  def collected?
+    @collected
+  end
+
+  def draw
+    Square.new(x: @x, y: @y, size: @size, color: @collected ? 'white' : 'yellow')
   end
 end
 
@@ -118,8 +151,13 @@ obstacles = [
 ]
 
 holes = [
-  { x: 250, y: 580, width: 100, height: 20, color: 'black' },
+  { x: 250, y: 580, width: 80, height: 20, color: 'black' },
   { x: 550, y: 580, width: 100, height: 20, color: 'black' }
+]
+points = [
+  { x: 100, y: 540, size: 20 }, 
+  { x: 350, y: 430, size: 20 }, 
+  { x: 140, y: 330, size: 20 }, 
 ]
 
 keys_pressed = { left: false, right: false }
@@ -137,11 +175,19 @@ update do
     Rectangle.new(x: hole[:x], y: hole[:y], width: hole[:width], height: hole[:height], color: hole[:color])
   end
 
+  points.each do |point|
+    unless point[:collected]
+      Point.new(point[:x], point[:y], point[:size]).draw 
+    end
+  end
+
   player.move_left if keys_pressed[:left]
   player.move_right if keys_pressed[:right]
 
   player.draw
-  player.update(obstacles, holes)
+  player.update(obstacles, holes, points)
+
+  score_text = Text.new("Score: #{player.score}", x: 10, y: 10, size: 20, color: 'white')
 end
 
 on :key_down do |event|
